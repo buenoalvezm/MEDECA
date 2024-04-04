97e29de2c5517e7340a0b01b2eb3eef01699ef7a
María Bueno Álvez <buenoalvezm@gmail.com>
2024-04-04 08:14:57 +0200

    Themes and palettes for MEDECA project

---
+++ scripts/themes_palettes.R
@@ -0,0 +1,68 @@
+library(RColorBrewer)
+
+# Palettes
+pal_panel <- c("#28C7E3",
+               "#FA1E00",
+               "#0A56A1",
+               "#FDC600")
+pal_de <-
+  c("not significant" = "#D3D3D3",
+    "significant up" = "#FF7176",
+    "significant down" = "#92C9DA")
+
+
+# Levels
+
+
+# Themes
+theme_hpa <- 
+  function(angled = F, axis_x = T, axis_y = T, facet_title = T) {
+    t <- 
+      theme(
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(),
+        panel.spacing = unit(0.2, "lines"), 
+        panel.background=element_rect(fill="white"),
+        panel.border = element_blank(),
+        plot.title = element_text(face = "bold",
+                                  size = rel(1), hjust = 0.5),
+        plot.subtitle=element_text(face = "bold",hjust = 0.5, size=rel(1),vjust=1),
+        axis.title = element_text(face = "bold",size = rel(1)),
+        axis.ticks.length = unit(.25, "cm"),
+        axis.line = element_line(linewidth = 0.5),
+        axis.text = element_text(size = rel(1), color = 'black'),
+        legend.key = element_blank(),
+        legend.position = "right",
+        legend.text = element_text(size=rel(0.8)),
+        legend.key.size= unit(0.7, "cm"),
+        legend.title = element_text(size=rel(1)),
+        plot.margin=unit(c(10,5,5,5),"mm"),
+        strip.background=element_rect(colour="grey90",fill="grey90"),
+        strip.text = element_text(face="bold")
+      )
+    
+    if(angled) {
+      t <- t + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) 
+    }
+    
+    if(axis_x == F) {
+      t <- t +
+        theme(axis.text.x = element_blank(),
+              axis.ticks.x = element_blank(),
+              axis.line.x = element_blank(),
+              axis.title.x = element_blank())
+    } 
+    
+    if(axis_y == F) {
+      t <- t +
+        theme(axis.text.y = element_blank(),
+              axis.ticks.y = element_blank(),
+              axis.line.y = element_blank(),
+              axis.title.y = element_blank())
+    }
+    if(facet_title == F) {
+      t <- t + theme(strip.text = element_blank())
+    }
+    return(t)
+  }
+

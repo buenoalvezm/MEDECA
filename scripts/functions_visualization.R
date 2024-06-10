9c5ea3bddf9efbca2bd3151fc883188351a29a1e
María Bueno Álvez <buenoalvezm@gmail.com>
2024-06-10 14:40:20 +0200

    change volcano threshold

--- scripts/functions_visualization.R
+++ scripts/functions_visualization.R
@@ -14,8 +14,8 @@ plot_volcano <- function(de_results) {
     geom_point(size = 1, alpha = 0.4, show.legend = F) + 
     geom_text_repel(data = labels, size = 2, show.legend = F) +
     geom_hline(yintercept = -log10(0.05), linetype = 'dashed', color = "darkgrey") +
-    geom_vline(xintercept = -1, linetype = 'dashed', color = "darkgrey") +
-    geom_vline(xintercept = 1, linetype = 'dashed', color = "darkgrey") +
+    geom_vline(xintercept = -0.5, linetype = 'dashed', color = "darkgrey") +
+    geom_vline(xintercept = 0.5, linetype = 'dashed', color = "darkgrey") +
     scale_color_manual(values = pal_de) +
     theme_hpa() +   
     theme(axis.text = element_text(size = 8),

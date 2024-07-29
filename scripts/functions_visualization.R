599cdf6a6e2eaf4165941dc5d816839210b8c872
María Bueno Álvez <buenoalvezm@gmail.com>
2024-07-29 13:56:52 +0200

    add pca and umap function

--- scripts/functions_visualization.R
+++ scripts/functions_visualization.R
@@ -1,6 +1,164 @@
-
 # Functions for visualization
+library(ggrepel)
+library(tidytext)
+library(embed)
+library(ggbeeswarm)
+library(patchwork)
+library(ggsci)
+library(eulerr)
+library(ggplotify)
+library(pheatmap)
+library(ggridges)
 
+## Generate PCA
+do_pca <- function(data,
+                   meta = NULL,
+                   variable = NULL,
+                   wide = T,
+                   impute = T,
+                   plots = F) {
+  if (wide) {
+    data_w <- data
+  } else {
+    data_w <-
+      data |>
+      select(Sample = DAid, Assay, NPX) |>
+      pivot_wider(values_from = NPX,
+                  names_from = Assay)
+  }
+  
+  if (impute) {
+    pca_rec <-
+      recipe( ~ ., data = data_w) %>%
+      update_role(Sample, new_role = "id")  |>
+      step_normalize(all_predictors()) |>
+      step_impute_knn(all_predictors()) |>
+      step_pca(all_predictors())
+    
+    pca_prep <- prep(pca_rec)
+    
+    tidied_pca <- tidy(pca_prep, 3)
+    
+  } else {
+    pca_rec <-
+      recipe( ~ ., data = data_w) %>%
+      update_role(Sample, new_role = "id")  |>
+      step_normalize(all_predictors()) |>
+      step_pca(all_predictors())
+    
+    pca_prep <- prep(pca_rec)
+    
+    tidied_pca <- tidy(pca_prep, 2)
+  }
+  loadings_data <-
+    tidied_pca |>
+    rename(Assay = terms,
+           Value = value,
+           PC = component)
+  
+  pca_res <-  juice(pca_prep)
+  
+  if (plots) {
+    # Loadings plot
+    loadings_plot <-
+      tidied_pca %>%
+      filter(component %in% paste0("PC", 1:4)) %>%
+      group_by(component) %>%
+      top_n(8, abs(value)) %>%
+      ungroup() %>%
+      mutate(terms = reorder_within(terms, abs(value), component)) %>%
+      ggplot(aes(abs(value), terms, fill = value > 0)) +
+      geom_col() +
+      facet_wrap( ~ component, scales = "free_y") +
+      scale_y_reordered() +
+      labs(x = "Absolute value of contribution",
+           y = NULL, fill = "Positive?") +
+      theme_hpa()
+    
+    # PCA plot
+    pca_plot <-
+      pca_res %>%
+      left_join(meta |> 
+                  rename(Sample = DAid), by = "Sample") %>%
+      ggplot(aes(PC1, PC2)) +
+      geom_point(aes(color = !!sym(variable)), alpha = 0.7, size = 2) +
+      labs(color = NULL) +
+      theme_hpa() +
+      labs(color = variable)
+    
+    return(
+      list(
+        "pca_res" = pca_res,
+        "loadings" = loadings_data,
+        "pca_plot" = pca_plot,
+        "loadings_plot" = loadings_plot
+      )
+    )
+  } else {
+    return(list("pca_res" = pca_res,
+                "loadings" = loadings_data))
+  }
+  
+}
+
+## Generate UMAP
+do_umap <- function(data,
+                    meta = NULL,
+                    variable = NULL,
+                    wide = T,
+                    impute = T,
+                    plots = F,
+                    n_neighbors = 15) {
+  if (wide) {
+    data_w <- data
+  } else {
+    data_w <-
+      data |>
+      select(Sample = DAid, Assay, NPX) |>
+      pivot_wider(values_from = NPX,
+                  names_from = Assay)
+  }
+  
+  if (impute) {
+    umap_rec <-
+      recipe( ~ ., data = data_w) %>%
+      update_role(Sample, new_role = "id")  |>
+      step_normalize(all_predictors()) |>
+      step_impute_knn(all_predictors()) |>
+      step_umap(all_predictors(), neighbors = n_neighbors)
+    
+    umap_prep <- prep(umap_rec)
+    
+  } else {
+    umap_rec <-
+      recipe( ~ ., data = data_w) %>%
+      update_role(Sample, new_role = "id")  |>
+      step_normalize(all_predictors()) |>
+      step_umap(all_predictors(), neighbors = n_neighbors)
+    
+    umap_prep <- prep(umap_rec)
+    
+  }
+  
+  umap_res <-  juice(umap_prep)
+  
+  if (plots) {
+    # Loadings plot
+    umap_plot <-
+      umap_res |>
+      left_join(meta |> 
+                  rename(Sample = DAid), by = "Sample") |>
+      ggplot(aes(UMAP1, UMAP2, color = !!sym(variable))) +
+      geom_point(alpha = 0.7, size = 2) +
+      theme_hpa()
+    
+    return(list("umap_res" = umap_res,
+                "umap_plot" = umap_plot))
+  } else {
+    return(umap_res)
+  }
+  
+}
 ## Generate volcano plot from differential expression results                                                                                                                                  
 plot_volcano <- function(de_results) {
   

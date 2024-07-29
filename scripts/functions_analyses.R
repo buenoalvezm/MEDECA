aeea55d886aa8249741b1af347afc801381fa1cb
María Bueno Álvez <buenoalvezm@gmail.com>
2024-07-30 00:32:46 +0200

    add lasso function

--- scripts/functions_analyses.R
+++ scripts/functions_analyses.R
@@ -7,21 +7,22 @@ library(tidymodels)
 do_limma <-
   function(data_wide, 
            metadata,
-           disease,
+           variable,
+           case,
            correct = T) {
     
     # Filter data (cohort, variable is 0 or 1)
     dat <-
       data_wide %>% 
       inner_join(metadata %>% 
-                   select(Sample, Sex, Age, BMI, Disease), by = "Sample") %>% 
-      rename(Group = Disease) %>% 
-      mutate(Group = ifelse(Group == disease, "1_Case", "0_Control")) 
+                   select(Sample, Sex, Age, !!sym(variable)), by = "Sample") %>% 
+      rename(Group = !!sym(variable)) %>% 
+      mutate(Group = ifelse(Group == case, "1_Case", "0_Control")) 
     
-    # Design a model - add Group, and Sex, Age, BMI
+    # Design a model - add Group, and Sex, Age
     if(correct == T) {
-      design <- model.matrix(~0 + as.factor(dat$Group) + as.factor(dat$Sex) + dat$Age ) #+ dat$BMI
-      colnames(design) <- c("control", "case",  "Sex", "Age") #, "BMI"
+      design <- model.matrix(~0 + as.factor(dat$Group) + as.factor(dat$Sex) + dat$Age) 
+      colnames(design) <- c("control", "case",  "Sex", "Age") 
     } else {
       design <- model.matrix(~0 + as.factor(dat$Group))
       colnames(design) <- c("control", "case")
@@ -32,7 +33,7 @@ do_limma <-
     
     dat_fit <- 
       dat %>% 
-      select(-Sex, -Age, -BMI, -Group)  %>% 
+      select(-Sex, -Age, -Group)  %>% 
       column_to_rownames("Sample") %>% 
       t()
     
@@ -54,11 +55,163 @@ do_limma <-
     DE_res <- 
       DE_results %>% 
       as_tibble(rownames = "Assay") %>% 
-      mutate(Disease = disease)
+      mutate(sig = case_when(adj.P.Val < 0.05 & logFC < -0.5 ~ "significant down",
+                             adj.P.Val < 0.05 & logFC > 0.5 ~ "significant up", 
+                             T ~ "not significant")) 
     
     return(DE_res)
   }
 
-do_lasso <- function() {
-  
-}
+do_lasso <-  
+  function(variable,
+           case, 
+           split_train, 
+           split_test) {
+    
+    # Prepare data - make custom split for current variable
+    training_dat <- 
+      split_train |> 
+      mutate(Class = case_when(!!sym(variable) == case ~ "Case",
+                               !!sym(variable) != case ~ "Control")) |> 
+      mutate(Class = factor(Class)) |> 
+      select(-!!sym(variable))
+    
+    testing_dat <- 
+      split_test  |> 
+      mutate(Class = case_when(!!sym(variable) == case ~ "Case",
+                               !!sym(variable) != case ~ "Control")) |> 
+      mutate(Class = factor(Class)) |> 
+      select(-!!sym(variable))
+    
+    ml_split_custom <- make_splits(training_dat, testing_dat)
+    
+    # Recipe with ML steps
+    ml_recipe <- 
+      recipe(Class ~ ., data = training_dat) |> 
+      update_role(Sample, new_role = "id") |> 
+      step_normalize(all_numeric()) |> 
+      step_nzv(all_numeric()) |> 
+      step_corr(all_numeric()) |> 
+      step_impute_knn(all_numeric()) 
+    
+    # LASSO model specifications
+    glmnet_specs <- 
+      logistic_reg() |> 
+      set_mode("classification") |> 
+      set_engine("glmnet") |> 
+      set_args(penalty = tune(), 
+               mixture = 1) 
+    
+    # ML workflow
+    glmnet_wflow <-
+      workflow() |> 
+      add_recipe(ml_recipe) |> 
+      add_model(glmnet_specs) 
+    
+    # Define glmnet grid
+    set.seed(213)
+    glmnet_grid <-
+      glmnet_wflow |>
+      extract_parameter_set_dials() |>
+      grid_latin_hypercube(size = 10)
+    
+    # Define the resamples (CV)
+    set.seed(213)
+    ml_rs <- vfold_cv(training_dat, v = 10, strata = Class)
+    
+    # Define the evaluation metrics (add brier)
+    eval_metrics <- metric_set(roc_auc)
+    
+    # Define control_grid
+    set.seed(213)
+    ctrl <- control_grid(save_pred = TRUE, parallel_over = "everything") # look at extract = identity
+    
+    # Glmnet grid search
+    set.seed(213)
+    glmnet_res <-
+      glmnet_wflow |>
+      tune_grid(
+        resamples = ml_rs,
+        grid = glmnet_grid,
+        control = ctrl,
+        metrics = eval_metrics
+      )
+
+    plot_train <- autoplot(glmnet_res)
+    
+    predictions_train <- 
+      glmnet_res |> 
+      collect_predictions()
+    
+    metrics_train <- 
+      glmnet_res |> 
+      collect_metrics()
+    
+    # Select best hyperparameter
+    best_glmnet <- 
+      select_best(glmnet_res, metric = "roc_auc") |> 
+      select(-.config)
+    
+    #Finalize the workflow and fit the final model
+    glmnet_wflow <- 
+      glmnet_wflow |>  
+      finalize_workflow(best_glmnet)
+    
+    final_glmnet_fit <- last_fit(glmnet_wflow, ml_split_custom, metrics = eval_metrics) 
+    
+    # Extract model performance
+    performance <- 
+      final_glmnet_fit |> 
+      collect_metrics() |> 
+      select(-.config, -.estimator)
+    
+    glmnet_auc <- 
+      final_glmnet_fit |> 
+      collect_metrics() |> 
+      filter(.metric == "roc_auc") |> 
+      pull(.estimate) |> 
+      round(2)
+    
+    # Extract protein importance
+    important_proteins <- 
+      final_glmnet_fit  |> 
+      extract_fit_parsnip() %>%
+      tidy() |> 
+      filter(term != "(Intercept)") |> 
+      arrange(-abs(estimate)) |> 
+      filter(abs(estimate) > 0) |> 
+      select(-penalty)
+    
+    # Extract model predictions
+    predictions <- 
+      final_glmnet_fit |> 
+      collect_predictions(summarize = F) 
+    
+    # Confusion matrix
+    cm <-
+      predictions |>
+      mutate(pred = ifelse(.pred_Control > 0.5, "Control", "Case"),
+             Class = ifelse(Class == "Control", "Control", "Case")) |>
+      mutate(Class = factor(Class, levels = c("Case", "Control")),
+             pred = factor(pred, levels = c("Case", "Control"))) |> 
+      conf_mat(Class, pred)
+    
+    # ROC curve
+    roc <- 
+      predictions |>
+      roc_curve(truth = Class, .pred_Case) 
+    
+    
+    return(list("penalty" = best_glmnet,
+                "glmnet_model" = glmnet_res,
+                "predictions_train" = predictions_train, 
+                "performance_train" = metrics_train,
+                "final_workflow" = glmnet_wflow,
+                "final_fit" = final_glmnet_fit,
+                "predictions" = predictions,
+                "performance" = performance,
+                "confusion_matrix" = cm,
+                "roc_curve" = roc, 
+                "important_proteins" = important_proteins))
+  }
+

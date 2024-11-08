43f9d3f2dc0d2fbec75071d5867436bfb70c0c0f
María Bueno Álvez <buenoalvezm@gmail.com>
2024-11-08 16:34:07 +0100

    add step_filter_t_test function

--- scripts/functions/functions_analyses.R
+++ scripts/functions/functions_analyses.R
@@ -71,24 +71,22 @@ do_limma <-
 # Function to select proteins based on AUC
 step_filter_auc <-  function(data, 
                              cutoff = 0.8) {
-  proteins <- 
-    data |> 
-    select(-Sample, -Cancer) |> 
-    colnames()
-  
+
   auc_res <- 
-    map_df(proteins, function(protein) {
+    map_df(unique(data$Assay), function(protein) {
     protein_exp <- 
       data |> 
-      pull(sym(protein))
+      filter(Assay == protein) |> 
+      pull(NPX)
     
     cancer_class <- 
       data |> 
+      filter(Assay == protein) |> 
       pull(Cancer)
     
-    res <- pROC::roc(cancer_class, protein_exp)
+    res <- pROC::roc(cancer_class, protein_exp, verbose = F)
     
-    tibble(Protein = protein,
+    tibble(Assay = protein,
            AUC = round(as.numeric(res$auc), 2))
     
   })
@@ -97,7 +95,9 @@ step_filter_auc <-  function(data,
     auc_res |> 
     ggplot(aes(AUC)) +
     geom_histogram() +
-    theme_hpa()
+    geom_vline(xintercept = cutoff, color = "red") +
+    theme_hpa() +
+    ggtitle("AUC distribution")
   
   auc_res_cutoff <- 
     auc_res |> 
@@ -107,24 +107,104 @@ step_filter_auc <-  function(data,
               auc_distribution = plot_auc,
               final_list = auc_res_cutoff))
   
+}
+
+
+step_filter_t_test <- 
+  function(data,
+           output = "NPX",
+           cutoff = 1) {
+    
+    ttest_results <- 
+      map_df(unique(data$Assay), function(protein) {
+        
+        group1<-
+          data %>%
+          filter(Assay == protein,
+                 Cancer == "Yes") %>%
+          pull(NPX)
+        
+        group2<-
+          data %>%
+          filter(Assay == protein,
+                 Cancer == "No") %>%
+          pull(NPX)
+        
+        test_res <- t.test(group1 ,group2)
+        p.val<- test_res$p.value
+        difference <- mean(group1)-mean(group2)
+        
+        tibble(Assay = protein, 
+               pval = p.val, 
+               NPX_difference = difference)
+        
+      }) |> 
+      mutate(pval_adj = p.adjust(pval, method = "BH")) 
+    
+    if(output == "NPX") {
+      
+      npx_res <- 
+        ttest_results |> 
+        select(Assay, NPX_difference)
+      
+      plot_npx <- 
+        npx_res |> 
+        ggplot(aes(NPX_difference)) +
+        geom_histogram() +
+        geom_vline(xintercept = cutoff, color = "red") +
+        theme_hpa() +
+        ggtitle("NPX difference distribution")
+      
+      npx_res_cutoff <- 
+        npx_res |> 
+        filter(NPX_difference > cutoff)
+      
+      return(list(full_list = npx_res,
+                  npx_distribution = plot_npx,
+                  final_list = npx_res_cutoff))
+      
+    } else if (output == "pval") {
+      
+      pval_res <- 
+        ttest_results |> 
+        select(Assay, pval_adj)
+      
+      plot_pval <- 
+        pval_res |> 
+        ggplot(aes(pval_adj)) +
+        geom_histogram() +
+        geom_vline(xintercept = cutoff, color = "red") +
+        ggtitle("p-value distribution") +
+        theme_hpa()
+      
+      pval_res_cutoff <- 
+        pval_res |> 
+        filter(pval_adj < cutoff)
+      
+      return(list(full_list = pval_res,
+                  pval_distribution = plot_pval,
+                  final_list = pval_res_cutoff))
+    
+    } else {
+      warning("Output not recognized")
+    }
+    
   }
 
 
 # Function to perform lasso analyses
-do_lasso <-  
+do_glmnet <-  
   function(variable,
            case, 
            split_train, 
-           split_test#,
-           #auc_filter = F
-           ) {
+           split_test) {
     
     # Prepare data - make custom split for current variable
     training_dat <- 
       split_train |> 
       mutate(Class = case_when(!!sym(variable) == case ~ "Case",
                                !!sym(variable) != case ~ "Control")) |> 
-      mutate(Class = factor(Class)) |> 
+      mutate(Class = factor(Class, levels = c("Control", "Case"))) |>  
       select(-!!sym(variable)) |> 
       mutate(Sample = as.character(Sample))
     
@@ -132,45 +212,21 @@ do_lasso <-
       split_test  |> 
       mutate(Class = case_when(!!sym(variable) == case ~ "Case",
                                !!sym(variable) != case ~ "Control")) |> 
-      mutate(Class = factor(Class)) |> 
+      mutate(Class = factor(Class, levels = c("Control", "Case"))) |>  # Explicitly set levels here
       select(-!!sym(variable))|> 
       mutate(Sample = as.character(Sample))
     
     
     ml_split_custom <- make_splits(training_dat, testing_dat)
-    
-    
-    # # Recipe with ML steps
-    # if(auc_filter == T) {
-    #   
-    #   # Find proteins to retain
-    #   proteins <- 
-    #     step_filter_auc(data = training_dat, 
-    #                     cutoff = 0.75)
-    #   
-    #   # Keep proteins in training and testing data
-    #   training_dat <- 
-    #     training_dat |> 
-    #     select(Sample, Class, proteins)
-    #   
-    #   testing_dat <- 
-    #     testing_dat |> 
-    #     select(Sample, Class, proteins)
-    #   
-    #   
-    # } else {
-    #   next
-    # }
-    # 
+  
     
     # Define recipe
     ml_recipe <- 
       recipe(Class ~ ., data = training_dat) |> 
-      step_relevel(Class, ref_level = "Control") |> 
+      #step_relevel(Class, ref_level = "Control") |> 
       update_role(Sample, new_role = "id") |> 
       step_normalize(all_numeric_predictors()) |> 
       step_nzv(all_numeric_predictors()) |> 
-      #step_corr(all_numeric_predictors()) |> 
       step_impute_knn(all_numeric_predictors()) 
     
     # LASSO model specifications
@@ -179,7 +235,7 @@ do_lasso <-
       set_mode("classification") |> 
       set_engine("glmnet") |> 
       set_args(penalty = tune(), 
-               mixture = tune()) #1 
+               mixture = 0) 
     
     # ML workflow
     glmnet_wflow <-
@@ -192,7 +248,7 @@ do_lasso <-
     glmnet_grid <-
       glmnet_wflow |>
       extract_parameter_set_dials() |>
-      grid_latin_hypercube(size = 30)
+      grid_latin_hypercube(size = 100)
     
     # Define the resamples (CV)
     set.seed(213)
@@ -230,7 +286,7 @@ do_lasso <-
     
     # Select best hyperparameter
     best_glmnet <- 
-      select_best(glmnet_res, metric = "roc_auc", event_level = "second") |> 
+      select_best(glmnet_res, metric = "roc_auc") |> 
       select(-.config)
     
     #Finalize the workflow and fit the final model
@@ -240,18 +296,17 @@ do_lasso <-
     
     final_glmnet_fit <- last_fit(glmnet_wflow,
                                  ml_split_custom, 
-                                 #event_level = "second",
                                  metrics = eval_metrics) 
     
     # Extract model performance
     performance <- 
       final_glmnet_fit |> 
-      collect_metrics(event_level = "second") |> 
+      collect_metrics() |> 
       select(-.config, -.estimator)
     
     glmnet_auc <- 
       final_glmnet_fit |> 
-      collect_metrics(event_level = "second") |> 
+      collect_metrics() |> 
       filter(.metric == "roc_auc") |> 
       pull(.estimate) |> 
       round(2)
@@ -299,6 +354,90 @@ do_lasso <-
                 "important_proteins" = important_proteins))
   }
 
+# Function to run protein selection and lasso models
+run_ml_seed <- 
+  function(seed = 213) {
+    
+    # Prepare split
+    set.seed(seed)
+    medeca_split <-
+      ml_data |> 
+      initial_split(prop = 0.7, strata = Cancer)
+    
+    medeca_train <- training(medeca_split)
+    medeca_test <- testing(medeca_split)
+    
+    # Select proteins
+    data_select_proteins <- 
+      data |> 
+      filter(Sample %in% medeca_train$Sample) |> 
+      left_join(metadata |> 
+                  select(Sample, Cancer), by = "Sample")
+    
+    selected_proteins_auc <- 
+      step_filter_auc(data = data_select_proteins,
+                      cutoff = 0.7)
+    
+    selected_proteins_npx <- 
+      step_filter_t_test(data = data_select_proteins,
+                         output = "NPX",
+                         cutoff = 1)
+    
+    selected_protein_pval <- 
+      step_filter_t_test(data = data_select_proteins,
+                         output = "pval",
+                         cutoff = 1E-6)
+    
+    y <- list("AUC > 0.7" = selected_proteins_auc$final_list$Assay, 
+              "NPX diff > 1" = selected_proteins_npx$final_list$Assay,
+              "adj pval < 1E-6" = selected_protein_pval$final_list$Assay)
+    
+    euler_plot <- plot(euler(y, shape = "circle"), quantities = TRUE, fills = list(fill = c("#449395", "#EA7C68", "#FDA638")),  alpha = 0.4) |> as.ggplot() 
+    
+    selected_protein_list <- union(union(y$`AUC > 0.7`, y$`NPX diff > 1`), y$`adj pval < 1E-6`)
+    
+    # Filter and calculate correlation
+    medeca_train_filtered <- 
+      medeca_train |> 
+      select(Sample, Cancer, selected_protein_list)
+    
+    cor_selected_proteins <- 
+      medeca_train_filtered |> 
+      select(-Cancer) |> 
+      column_to_rownames("Sample") |> 
+      cor(method = "spearman", use = "complete.obs")
+    
+    cor_plot <- 
+      cor_selected_proteins |> 
+      pheatmap()
+    
+    medeca_test_filtered <- 
+      medeca_test |> 
+      select(Sample, Cancer, selected_protein_list)
+    
+    # # Run lasso pipeline with selected proteins
+    # ml_medeca_filter<- 
+    #   do_lasso(variable = "Cancer",
+    #            case = "Yes", 
+    #            split_train = medeca_train_filtered, 
+    #            split_test = medeca_test_filtered) 
+    # 
+    # #saveRDS(ml_medeca_filter, savepath_data("ML", "ml_medeca_filter.rds"))
+    # 
+    # protein_importance <- 
+    #   ml_medeca_filter$important_proteins |> 
+    #   filter(Importance > 0)
+    # 
+    
+    
+    
+    return(list("selected_proteins_euler" = euler_plot,
+                "selected_proteins" = selected_protein_list,
+                "selected_proteins_correlation" = cor_plot))#,
+    #"protein_importance" = protein_importance,
+    # "performance" = ml_medeca_filter$performance)
+    
+  }
 
 # Function to join data from HPA and MEDECA
 combine_data <- 
